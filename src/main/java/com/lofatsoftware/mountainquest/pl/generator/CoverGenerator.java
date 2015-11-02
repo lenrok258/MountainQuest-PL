fc9bd4b31e8a8fc9e41d4326efbd263133bb93b0
kornel.zemla <kornel.zemla@ocado.com>
2015-11-02 16:55:03 +0100

    Cover generator

--- src/main/java/com/lofatsoftware/mountainquest/pl/generator/CoverGenerator.java
+++ src/main/java/com/lofatsoftware/mountainquest/pl/generator/CoverGenerator.java
@@ -1,5 +1,6 @@
 package com.lofatsoftware.mountainquest.pl.generator;
 
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
@@ -9,6 +10,7 @@ import com.itextpdf.text.PageSize;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.lofatsoftware.mountainquest.pl.generator.page.utils.ImageUtils;
 import com.lofatsoftware.mountainquest.pl.generator.page.utils.PhraseUtil;
 
 import java.io.File;
@@ -20,7 +22,7 @@ import java.util.List;
 public class CoverGenerator {
 
     private static final String RESULT_FILE = "mountain-quest_covers.pdf";
-    private static final int PAGE_MARGIN = 20;
+    private static final int COVER_MARGIN = 10;
 
     private final Document document;
     private final PdfWriter pdfWriter;
@@ -28,14 +30,14 @@ public class CoverGenerator {
 
     public CoverGenerator(List<File> coverFiles) throws FileNotFoundException, DocumentException {
         this.coverFiles = coverFiles;
-        this.document = new Document(PageSize.A4.rotate(), PAGE_MARGIN, PAGE_MARGIN, PAGE_MARGIN, PAGE_MARGIN);
+        this.document = new Document(PageSize.A4.rotate(), 0, 0, 0, 0);
         this.pdfWriter = PdfWriter.getInstance(document, new FileOutputStream(RESULT_FILE));
         this.document.open();
     }
 
     public void generatePdf() throws IOException, DocumentException {
-
         for (File coverFile : coverFiles) {
+            System.out.println("Generating cover from file: " + coverFile.getName());
             document.add(generateCoverPage(coverFile));
             document.newPage();
         }
@@ -46,11 +48,19 @@ public class CoverGenerator {
     private PdfPTable generateCoverPage(File coverFile) throws IOException, DocumentException {
 
         Image image = Image.getInstance(coverFile.toURL());
+        float heightToWidthRatio = (210f / 297f);
+        Image imageCropped = ImageUtils.cropImageToMeetRatio(pdfWriter, image, heightToWidthRatio);
 
-        PdfPCell cell = new PdfPCell(image);
+        PdfPCell cell = new PdfPCell(imageCropped, true);
+        //cell.setBackgroundColor(BaseColor.YELLOW);
         cell.setBorder(0);
+        cell.setPadding(COVER_MARGIN);
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        cell.setExtraParagraphSpace(0);
+        cell.setRightIndent(0);
 
-        PdfPTable table = new PdfPTable(1);
+        PdfPTable table = new PdfPTable(1);;
         table.setWidthPercentage(100f);
         table.setWidths(new int[]{1});
         table.setExtendLastRow(true);
