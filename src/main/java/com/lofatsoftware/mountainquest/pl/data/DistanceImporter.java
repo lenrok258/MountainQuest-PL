1b7a789360ba8648ef8abd1d66a8321e574d7c10
kornel.zemla <kornel.zemla@ocado.com>
2016-03-22 16:32:04 +0100

    Get distance retries

--- src/main/java/com/lofatsoftware/mountainquest/pl/data/DistanceImporter.java
+++ src/main/java/com/lofatsoftware/mountainquest/pl/data/DistanceImporter.java
@@ -2,24 +2,22 @@ package com.lofatsoftware.mountainquest.pl.data;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.text.MessageFormat;
 import java.util.Optional;
 
+import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
 public class DistanceImporter {
 
     private static final String DISTANCE_API_URL_TEMPLATE =
-            "https://maps.googleapis.com/maps/api/distancematrix/json?origins=50.0768693,19" +
-                    ".9028439&traffic_model=optimistic&destinations={0},{1}";
+            "https://maps.googleapis.com/maps/api/distancematrix/json?origins=50.0768693,19.9028439&destinations={0},{1}";
     private static final String DISTANCE_LOCAL_FILE_NAME = "distance.json";
 
     private final File dataDirectory;
@@ -33,7 +31,8 @@ public class DistanceImporter {
     }
 
     public String getDistanceFromCracow() throws IOException {
-        return getLocalFileDistanceValue().orElseGet(this::downloadDistance);
+        return getLocalFileDistanceValue()
+                .orElseGet(() -> downloadDistanceToFileAndReturn());
     }
 
     private Optional<String> getLocalFileDistanceValue() throws IOException {
@@ -45,24 +44,44 @@ public class DistanceImporter {
                 : Optional.empty();
     }
 
-    private String downloadDistance() {
+    private String downloadDistanceToFileAndReturn() {
         try {
-            System.out.println("Downloading distance from Google API to " + dataDirectory.getPath());
-            URL distanceUrl = getDistanceAPIUrl(longitude, latitude);
+            String jsonPayload = downloadDistanceJsonWithRetries();
 
-            Path localFilePath = localDistanceFilePath(dataDirectory);
+            return "trolo";
+           /* Path localFilePath = localDistanceFilePath(dataDirectory);
+
 
             try (InputStream in = distanceUrl.openStream()) {
                 Files.copy(in, localFilePath, StandardCopyOption.REPLACE_EXISTING);
             }
-            return readValueFromFile(localFilePath.toAbsolutePath().toString());
+            return readValueFromFile(localFilePath.toAbsolutePath().toString());*/
         } catch (Exception e) {
             throw new IllegalStateException("Unable to download distance", e);
         }
     }
 
-    private URL getDistanceAPIUrl(String longitude, String latitude) throws MalformedURLException {
+    private String downloadDistanceJsonWithRetries() throws IOException {
+        for (int i = 0; i < 10; i++) {
+            float newLatitude = Float.parseFloat(latitude) + (0.01f * i);
+            String jsonPayload = downloadDistanceJson(String.valueOf(newLatitude), longitude);
+            JSONObject jsonObject = new JSONObject(jsonPayload);
+            if (hasCorrectResults(jsonObject)) {
+                return jsonPayload;
+            }
+        }
+        throw new IllegalStateException("Unable to find distance");
+    }
+
+    private String downloadDistanceJson(String latitude, String longitude) throws IOException {
+        System.out.println("Downloading distance from Google API to " + dataDirectory.getPath());
+        URL distanceUrl = getDistanceAPIUrl(latitude, longitude);
+        return IOUtils.toString(distanceUrl.openStream());
+    }
+
+    private URL getDistanceAPIUrl(String latitude, String longitude) throws MalformedURLException {
         String urlString = MessageFormat.format(DISTANCE_API_URL_TEMPLATE, latitude, longitude);
+        System.out.println(urlString);
         return new URL(urlString);
     }
 
@@ -76,15 +95,15 @@ public class DistanceImporter {
         String fileContent = new String(Files.readAllBytes(Paths.get(uri)));
 
         JSONObject jsonObject = new JSONObject(fileContent);
-        if (noResults(jsonObject)) {
-            // re try with different coordinates
+        if (hasCorrectResults(jsonObject)) {
+
         }
 
         return getDistanceValue(jsonObject);
     }
 
-    private boolean noResults(JSONObject jsonObject) {
-        return jsonObject
+    private boolean hasCorrectResults(JSONObject jsonObject) {
+        return !jsonObject
                 .getJSONArray("rows").getJSONObject(0)
                 .getJSONArray("elements").getJSONObject(0)
                 .getString("status").equals("ZERO_RESULTS");
@@ -95,6 +114,6 @@ public class DistanceImporter {
                 .getJSONArray("rows").getJSONObject(0)
                 .getJSONArray("elements").getJSONObject(0)
                 .getJSONObject("distance").getInt("value");
-        return String.valueOf(meters / 100);
+        return String.valueOf(meters / 1000);
     }
 }
